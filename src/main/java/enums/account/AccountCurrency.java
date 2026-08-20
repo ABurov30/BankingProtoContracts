6506cbeaaeb0ddf77a0fe103dec393c592db9a1d
Andrey <andrey-valeriy.burov@mail.ru>
2026-08-20 16:41:33 +0300

    feat: migration to minor units in transaction

--- src/main/java/enums/account/AccountCurrency.java
+++ src/main/java/enums/account/AccountCurrency.java
@@ -1,8 +1,31 @@
 package enums.account;
 
 public enum AccountCurrency {
-  USD,
-  EUR,
-  CNY,
-  GBP
+
+  USD("USD", 840, 2),
+  EUR("EUR", 978, 2),
+  CNY("CNY", 156, 2),
+  GBP("GBP", 826, 2);
+
+  private final String alphabeticCode;
+  private final int numericCode;
+  private final int minorUnit;
+
+  AccountCurrency(String alphabeticCode, int numericCode, int minorUnit) {
+    this.alphabeticCode = alphabeticCode;
+    this.numericCode = numericCode;
+    this.minorUnit = minorUnit;
+  }
+
+  public String getAlphabeticCode() {
+    return alphabeticCode;
+  }
+
+  public int getNumericCode() {
+    return numericCode;
+  }
+
+  public int getMinorUnit() {
+    return minorUnit;
+  }
 }
